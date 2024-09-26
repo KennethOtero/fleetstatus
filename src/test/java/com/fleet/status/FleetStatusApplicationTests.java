48da8cb9ba710affdb608b2d26aa8efd78077dec
Kenny <keotero99@gmail.com>
2024-09-26 19:39:16 -0400

    Testing failed checks

--- src/test/java/com/fleet/status/FleetStatusApplicationTests.java
+++ src/test/java/com/fleet/status/FleetStatusApplicationTests.java
@@ -3,11 +3,18 @@ package com.fleet.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class FleetStatusApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failedTest() {
+		fail("This is a failed test.");
 	}
 
 }
