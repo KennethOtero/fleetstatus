0a521b2d2594ef208a0717932fc098728fb4c75a
Kenny <keotero99@gmail.com>
2025-02-16 14:20:44 -0500

    FS-29: Removed Aircraft interfaces

--- src/main/java/com/fleet/status/service/impl/AircraftService.java
+++ src/main/java/com/fleet/status/service/impl/AircraftService.java
@@ -1,43 +1,39 @@
 package com.fleet.status.service.impl;
 
-import com.fleet.status.dao.impl.AircraftDAO;
+import com.fleet.status.dao.repository.AircraftRepository;
 import com.fleet.status.entity.Aircraft;
-import com.fleet.status.service.IAircraftService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Profile("dev")
 @RequiredArgsConstructor
-public class AircraftService implements IAircraftService {
+public class AircraftService {
 
-    private final AircraftDAO aircraftDAO;
+    private final AircraftRepository aircraftRepository;
 
-    @Override
     public Aircraft findById(int id) {
-        return aircraftDAO.findById(id);
+        Optional<Aircraft> foundAircraft = aircraftRepository.findById(id);
+        return foundAircraft.orElse(null);
     }
 
-    @Override
     public void save(Aircraft aircraft) throws Exception {
-        aircraftDAO.save(aircraft);
+        aircraftRepository.save(aircraft);
     }
 
-    @Override
     public List<Aircraft> getAllAircraftFromCarrier(int carrierId) {
-        return List.of();
+        return aircraftRepository.getAllAircraftFromCarrier(carrierId);
     }
 
-    @Override
     public void deleteAircraft(int aircraftId) {
-        aircraftDAO.deleteAircraft(aircraftId);
+        aircraftRepository.deleteAircraft(aircraftId);
     }
 
-    @Override
     public List<Aircraft> findAll() {
-        return aircraftDAO.findAll();
+        return (List<Aircraft>) aircraftRepository.findAll();
     }
 }
