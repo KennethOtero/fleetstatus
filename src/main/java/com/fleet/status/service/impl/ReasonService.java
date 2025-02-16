44d4ed919f9431606d6301a7f1d2dab3c86982ec
Kenny <keotero99@gmail.com>
2025-02-16 14:31:46 -0500

    FS-29: Removed Reason interfaces

--- src/main/java/com/fleet/status/service/impl/ReasonService.java
+++ src/main/java/com/fleet/status/service/impl/ReasonService.java
@@ -1,8 +1,7 @@
 package com.fleet.status.service.impl;
 
-import com.fleet.status.dao.IReasonDAO;
+import com.fleet.status.dao.repository.ReasonRepository;
 import com.fleet.status.entity.Reason;
-import com.fleet.status.service.IReasonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -11,22 +10,11 @@ import java.util.List;
 @Service
 @Profile("dev")
 @RequiredArgsConstructor
-public class ReasonService implements IReasonService {
+public class ReasonService {
 
-    private final IReasonDAO reasonDAO;
+    private final ReasonRepository reasonRepository;
 
-    @Override
-    public void save(Reason reason) throws Exception {
-
-    }
-
-    @Override
-    public Reason findReasonById(int id) {
-        return null;
-    }
-
-    @Override
     public List<Reason> getAllReason() {
-        return reasonDAO.getAllReason();
+        return (List<Reason>) reasonRepository.findAll();
     }
 }
