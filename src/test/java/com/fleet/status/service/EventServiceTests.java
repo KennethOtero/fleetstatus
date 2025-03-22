b6d8c754c71c75b8a03f21ebb3eb5d517d69ccf8
Kenny <keotero99@gmail.com>
2025-03-22 11:53:48 -0400

    FS-39: Adding base class for Event service tests

---
+++ src/test/java/com/fleet/status/service/EventServiceTests.java
@@ -0,0 +1,72 @@
+package com.fleet.status.service;
+
+import com.fleet.status.dao.EventDAO;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EventServiceTests {
+
+    @InjectMocks
+    private EventService eventService;
+
+    @Mock
+    private EventDAO eventDAO;
+
+    @Test
+    void testSaveEvent() {
+
+    }
+
+    @Test
+    void testFindEventById() {
+
+    }
+
+    @Test
+    void testGetHomepageEvents() {
+
+    }
+
+    @Test
+    void testGetOutOfServiceEvents() {
+
+    }
+
+    @Test
+    void testShowBackInService() {
+
+    }
+
+    @Test
+    void testUpdateEvent() {
+
+    }
+
+    @Test
+    void testPatchEvent() {
+
+    }
+
+    @Test
+    void testGenerateCSV() {
+
+    }
+
+    @Test
+    void testGenerateDownTimeReport() {
+
+    }
+
+    @Test
+    void testCalculateTimeByMinute() {
+
+    }
+
+    @Test
+    void testGetFilteredEvents() {
+
+    }
+}
