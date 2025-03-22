fb4bbf4a8b8b82c411e51ccdec2c69382b8b2da4
Kenny <keotero99@gmail.com>
2025-03-22 11:36:22 -0400

    FS-39: Adding aircraft service tests

---
+++ src/test/java/com/fleet/status/service/AircraftServiceTests.java
@@ -0,0 +1,60 @@
+package com.fleet.status.service;
+
+import com.fleet.status.dao.repository.AircraftRepository;
+import com.fleet.status.entity.Aircraft;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+public class AircraftServiceTests {
+
+    @InjectMocks
+    private AircraftService aircraftService;
+
+    @Mock
+    private AircraftRepository aircraftRepository;
+
+    @Test
+    public void testSave() {
+        Aircraft aircraft = new Aircraft();
+        aircraft.setAircraftId(1L);
+        when(aircraftRepository.save(any(Aircraft.class))).thenReturn(aircraft);
+
+        aircraftService.save(aircraft);
+
+        verify(aircraftRepository, times(1)).save(aircraft);
+    }
+
+    @Test
+    public void testDeleteAircraft() {
+        int aircraftId = 1;
+        doNothing().when(aircraftRepository).deleteAircraft(aircraftId);
+
+        aircraftService.deleteAircraft(aircraftId);
+
+        verify(aircraftRepository, times(1)).deleteAircraft(aircraftId);
+    }
+
+    @Test
+    public void testFindAll() {
+        Aircraft aircraft1 = new Aircraft();
+        aircraft1.setAircraftId(1L);
+        Aircraft aircraft2 = new Aircraft();
+        aircraft2.setAircraftId(2L);
+        List<Aircraft> aircraftList = Arrays.asList(aircraft1, aircraft2);
+        when(aircraftRepository.findAll()).thenReturn(aircraftList);
+
+        List<Aircraft> result = aircraftService.findAll();
+
+        assertEquals(2, result.size());
+        verify(aircraftRepository, times(1)).findAll();
+    }
+}
